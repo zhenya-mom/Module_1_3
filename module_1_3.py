b8aa9c264bfcbad04bdbb5147e4fbb015184af93
zhenya-mom <zhen.lemesh@icloud.com>
2024-10-04 19:53:02 +0300

    1st commit

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+from tkinter.font import names
+
+name = "Evgeniya"
+print ("Name:", name )
+age = 44
+print ("Age:", age)
+age = age + 1
+print ("New age:", age)
+is_student = True
+print ("Is student:", is_student)
